52d78662eaf4a82fdfd45fb55fca005b6cf1ceb0
0xAyoub <ayoubusiness1@gmail.com>
2023-03-09 00:38:53 +0100

    Modification du Footer

--- frontend/components/Footer/Footer.jsx
+++ frontend/components/Footer/Footer.jsx
@@ -5,20 +5,19 @@ import { uploadFileToIPFS, uploadJSONToIPFS } from "../../src/pinata";
 import Contract from "../../src/Snyker.json";
 import { Sneaker } from "../Sneaker/Sneaker"
 import axios from "axios";
-import ethers from "ethers"
 // import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export const Footer = () => {
 
+    
     const contractAddress = process.env.NEXT_PUBLIC_SCADDRESS
-
+    const ethers = require("ethers");
 
 
     const withdrawAll = async() => {
-        const ethers = require("ethers");
+       
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
-
         let contract = new ethers.Contract(contractAddress, Contract.abi, signer)
         let tx = await contract.withdrawAll()
         await tx.wait()
@@ -31,9 +30,8 @@ export const Footer = () => {
 
             <Text margin="13px" fontSize="13px">Copyright 2023 - Ayoub Benouda</Text>
 
-
-            <a href="/"> <Button colorScheme="red" onClick={withdrawAll} >Admin Withdraw</Button> </a>
-         
+                    <a href="/"> <Button colorScheme="red" onClick={withdrawAll} >Admin Withdraw</Button> </a>
+     
          </Flex>
     )
 }
